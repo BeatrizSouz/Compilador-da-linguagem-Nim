e06b04c99f3c20d97f4022e0db2f989d135f87c9
Davi Cardoso <122702586+davicardoso5524@users.noreply.github.com>
2025-07-30 20:04:39 -0300

    Update LexicoNim.py
    Atualização no Léxico, com base na nova documentação do léxico

--- mds/LexicoNim.py
+++ mds/LexicoNim.py
@@ -1,96 +1,92 @@
 import ply.lex as lex
 
 reservadas = {
-    'addr': 'ADDR', 
-    'and':'AND',
-    'as':'AS',
-    'asm':'ASM',
-    'bind':'BIND',
-    'block':'BLOCK',
-    'break':'BREAK',
-    'case':'CASE',
-    'cast': 'CAST', 
-    'concept': 'CONCEPT',
-    'const': 'CONST',
-    'continue': 'CONTINUE',
-    'converter': 'CONVERTER',
-    'defer': 'DEFER',
-    'discard': 'DISCARD',
-    'distinct': 'DISTINCT',
-    'div': 'DIV',
-    'do': 'DO',
-    'elif': 'ELIF',
-    'else': 'ELSE',
-    'end': 'END',
-    'enum': 'ENUM',
-    'except': 'EXCEPT',
-    'export': 'EXPORT',
-    'finally': 'FINALLY',
-    'for': 'FOR',
-    'from': 'FROM',
-    'func': 'FUNC',
-    'if': 'IF',
-    'import': 'IMPORT',
-    'in': 'IN',
-    'include': 'INCLUDE',
-    'interface': 'INTERFACE',
-    'iterator': 'ITERATOR',
-    'let': 'LET',
-    'macro': 'MACRO',
-    'mixin': 'MIXIN',
-    'mod': 'MOD',
-    'nil': 'NIL',
-    'not': 'NOT',
-    'notin': 'NOTIN',
-    'or': 'OR',
-    'out': 'OUT',
-    'proc': 'PROC',
-    'ptr': 'PTR',
-    'raise': 'RAISE',
-    'return': 'RETURN',
-    'shl': 'SHL',
-    'shr': 'SHR',
-    'static': 'STATIC',
-    'template': 'TEMPLATE',
-    'try': 'TRY',
-    'tuple': 'TUPLE',
-    'type': 'TYPE',
-    'using': 'USING',
-    'var': 'VAR',
-    'when': 'WHEN',
-    'while': 'WHILE',
-    'xor': 'XOR',
-    'yield': 'YIELD',
-    'true': 'TRUE',
-    'false': 'FALSE'
+    'if': 'IF', 'else': 'ELSE', 'while': 'WHILE', 'for': 'FOR',
+    'proc': 'PROC', 'var': 'VAR', 'type': 'TYPE',
+    'return': 'RETURN', 'import': 'IMPORT'
 }
 
-    
-tokens = ['PLUS','MINUS','TIMES','DIVIDE','CARET','EXPONENT']+list(reservadas.values())
+tokens = [
+    'ID', 'NUMBER', 'STRING',
+    'SOMA', 'SUB', 'MUL', 'DIV', 'MOD', 'EXP',
+    'ATRIB', 'ADICIGUAL', 'SUBIGUAL',
+    'PV', 'VIRG', 'LPAREN', 'RPAREN',
+    'LCHAV', 'RCHAV', 'LCOLCH', 'RCOLCH', 'DOISPONTOS'
+] + list(reservadas.values())
 
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
-t_CARET = r'\^'
-t_EXPONENT = r'\*'
+t_SOMA       = r'\+'
+t_SUB        = r'-'
+t_MUL        = r'\*'
+t_DIV        = r'/'
+t_MOD        = r'\bmod\b'
+t_EXP        = r'\^'
+t_ATRIB      = r'='
+t_ADICIGUAL  = r'\+='
+t_SUBIGUAL   = r'-='
+t_PV         = r';'
+t_VIRG       = r','
+t_LPAREN     = r'\('
+t_RPAREN     = r'\)'
+t_LCHAV      = r'\{'
+t_RCHAV      = r'\}'
+t_LCOLCH     = r'\['
+t_RCOLCH     = r'\]'
+t_DOISPONTOS = r':'
 
-t_ignore = ' \t' #ignora espaço e tabulação
+def t_ID(t):
+    r'`?[a-zA-Z][a-zA-Z_0-9]*`?'
+    lexema = t.value.strip('`')
+    if lexema in reservadas and not (t.value.startswith('`') and t.value.endswith('`')):
+        t.type = reservadas[lexema]
+    else:
+        t.type = 'ID'
+        t.value = lexema
+    return t
 
-def t_newline(t): #atualiza a linha quando \n
-  r'\n+'
-  t.lexer.lineno += len(t.value)
+def t_NUMBER(t):
+    r'0[xX][0-9a-fA-F]+|0[bB][01]+|0[oO][0-7]+|\d+\.\d+|\d+'
+    if t.value.startswith(('0x', '0X')):
+        t.value = int(t.value, 16)
+    elif t.value.startswith(('0b', '0B')):
+        t.value = int(t.value, 2)
+    elif t.value.startswith(('0o', '0O')):
+        t.value = int(t.value, 8)
+    elif '.' in t.value:
+        t.value = float(t.value)
+    else:
+        t.value = int(t.value)
+    return t
+
+def t_STRING(t):
+    r'(\"\"\"(.|\n)*?\"\"\"|\"(\\.|[^\\"])*\")'
+    t.value = t.value[1:-1]
+    return t
+
+def t_comment_multiline(t):
+    r'\#\[(.|\n)*?\]\#'
+    pass
+
+def t_comment_line(t):
+    r'\#.*'
+    pass
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+t_ignore = ' \t\r'
 
 def t_error(t):
-  print("Illegal character '%s'" % t.value[0])
-  t.lexer.skip(1)
+    print(f"Caractere ilegal: '{t.value[0]}' na linha {t.lineno}")
+    t.lexer.skip(1)
 
+def main():
+    f = open("input1.nim", "r")
+    lexer = lex.lex(debug=1)
+    lexer.input(f.read())
+    print('\n\n# lexer output:')
+    for tok in lexer:
+        print('type:', tok.type, ', value:', tok.value)
 
-
-lexer = lex.lex()
-# teste
-
-lexer.input("+\n  - --+\n +  +**/(^^)")
-print('{:10s}{:10s}{:10s}{:10s}'.format("Token", "Lexema", "Linha", "Coluna"))
-for tok in lexer:
-  print('{:10s}{:10s}{:10s}{:10s}'.format(tok.type, tok.value, str(tok.lineno), str(tok.lexpos)))
+if __name__ == "__main__":
+    main()
